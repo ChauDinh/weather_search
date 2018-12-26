ffd693035efb2ebc9250720a6b6cf91a2281c889
ChauDinh <chaudvb95uc@gmail.com>
2018-12-26 14:09:16 +0700

    Initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <p>© created by Chau Dinh, 2018</p>
+        <br/>
+        <br/>
+        <br/>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <p></p>
+        <p></p>
+        <h4>Find out temperature, conditions and more...</h4>
+    </div>
+);
+
+export default Titles;
